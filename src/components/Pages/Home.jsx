4a02185f3a35ca03a4e513859be28b40a6590b03
TONYGUDIMA <gudima.anton@yahoo.com>
2023-07-05 15:33:01 +0300

    Update Home.jsx

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -4,10 +4,10 @@ function Home() {
   return (
     <div className={css.container}>
       <h1>HomePage</h1>
+      <p>This is my PhoneBook App with registration and contact management.</p>
       <p>
-        This is my PhoneBook App with registration and contact management. You
-        can sign up by clicking SignUp button or LogIn button if u already have
-        an account
+        You can sign up by clicking SignUp button or LogIn button if u already
+        have an account.
       </p>
     </div>
   );
