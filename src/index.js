5f4e4282f592c1e4b6780c5ab473922586096ac0
TONYGUDIMA <gudima.anton@yahoo.com>
2023-07-07 00:58:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store, persistor } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </PersistGate>
