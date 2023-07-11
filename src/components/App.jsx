ddc2dd392f390db24d13d3acd0259f15f9110e7b
TONYGUDIMA <gudima.anton@yahoo.com>
2023-07-11 23:58:09 +0300

    )

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,13 @@ import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contactsApi/operations';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 import Home from './Pages/Home';
 import Contacts from './Pages/Contacts';
 export const App = () => {
   const userToken = useSelector(state => state.auth.token);
+
   const dispatch = useDispatch();
   useEffect(() => {
     const getUser = async () => {
@@ -26,9 +29,27 @@ export const App = () => {
       <AppBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        {!userToken && <Route path="/login" element={<LogIn />}></Route>}
+        {/* {!userToken && <Route path="/login" element={<LogIn />}></Route>}
         {!userToken && <Route path="/signUp" element={<SignUp />}></Route>}
-        <Route path="/contacts" element={<Contacts />}></Route>
+        {userToken && <Route path="/contacts" element={<Contacts />}></Route>} */}
+        <Route
+          path="/signUp"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<SignUp />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LogIn />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
       </Routes>
     </>
   );
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -4,6 +4,7 @@ const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
+  isRefreshing: false,
 };
 const authSlice = createSlice({
   name: 'auth',
